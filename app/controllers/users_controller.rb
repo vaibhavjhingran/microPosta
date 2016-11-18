d95ebf87917293e8f2127aa5f061c4517147ea53
Vaibhav Jhingran <vaibhavjhingran@gmail.com>
2016-11-18 23:40:40 +0530

    Bug fix for regex + styling + creating user sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
   def new
   end
+
+  def show
+  	@user = User.find(paramsp[:id])
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,12 +2,12 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
 	def setup
-		@user = User.new(name: 'Sample User', email:'user@example.com',
+		@user = User.new(name: 'Sample User Name', email:'usertimes@example.com',
 						password: "foobar", password_confirmation: "foobar")
 	end
 
 	test "should be valid" do
-		assert_not @user.valid?
+		assert @user.valid?
 	end
 
 	test "name should be present" do
@@ -30,14 +30,14 @@ class UserTest < ActiveSupport::TestCase
 		assert_not @user.valid?
 	end
 
-	# test "email validation should accept valid addresses" do
-	# 	valid_addresses = %w[ice@sample.com USER@foo.COM A_US-ER@foo.bar.org
-	# 					 first.last@foo.jp alice+bob@baz.cn]
- #    	valid_addresses.each do |valid_address|
- #      	@user.email = valid_address
- #      	assert @user.valid?, "#{valid_address.inspect} should be valid"
- #    	end
-	# end
+	test "email validation should accept valid addresses" do
+		valid_addresses = %w[ice@sample.com USER@foo.COM A_US-ER@foo.bar.org
+						 first.last@foo.jp alice+bob@baz.cn]
+    	valid_addresses.each do |valid_address|
+      	@user.email = valid_address
+      	assert @user.valid?, "#{valid_address.inspect} should be valid"
+    	end
+	end
 
 	test "email validation should reject invalid addresses" do
     	invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
@@ -55,12 +55,12 @@ class UserTest < ActiveSupport::TestCase
   		assert_not duplicate_user.valid?
   	end
 
-  	# test "email should be lower-case" do
-  	# 	mixed_case_email = "Foo@eXamPlE.CoM"
-  	# 	@user.email = mixed_case_email
-  	# 	@user.save
-  	# 	assert_equal mixed_case_email.downcase, @user.reload.email
-  	# end
+  	test "email should be lower-case" do
+  		mixed_case_email = "Foo@eXamPlE.CoM"
+  		@user.email = mixed_case_email
+  		@user.save
+  		assert_equal mixed_case_email.downcase, @user.reload.email
+  	end
 
   	test "password should be present" do
   		@user.password = @user.password_confirmation = ' ' * 7
