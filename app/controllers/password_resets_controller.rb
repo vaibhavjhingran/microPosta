02de3b0c64aa34efc1ea7cd31fbda15535d148d2
Vaibhav Jhingran <vaibhavjhingran@gmail.com>
2016-11-25 16:35:41 +0530

    Adding password reset checks & code

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -2,6 +2,7 @@ class PasswordResetsController < ApplicationController
   
   before_action :get_user, only: [:edit, :update]
   before_action :valid_user, only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update]
 
   def new
   end
@@ -22,7 +23,23 @@ class PasswordResetsController < ApplicationController
   	end
   end
 
+  def update
+  	if params[:user][:password].empty?
+  		@user.errors.add(:password, "Can't be empty.")
+  		render 'edit'
+  	elsif @user.update_attributes(user_params)
+  		log_in @user
+  		flash[:success] = "Password has been reset."
+  		redirect_to @user
+  	else
+  		render 'edit'
+  end
+
   private
+  	def user_params
+  		params.require(:user).permit(:password, :password_confirmation)
+  	end
+
     def get_user
     	@user = User.find_by(email: params[:email])
     end
@@ -31,5 +48,12 @@ class PasswordResetsController < ApplicationController
     	unless (@user && @user.activated? && @user.authenticated?(:reset, params[:id]))
     		redirect_to root_url
     	end 
+    end
+
+    def check_expiration
+    	if @user.password_reset_expired?
+    		flash[:danger] = "Password reset has expired."
+    		redirect_to new_password_reset_url
+    	end
     end
 end
