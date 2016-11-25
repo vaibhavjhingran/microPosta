52a28a907666031651d4679e68194aa4eb74c152
Vaibhav Jhingran <vaibhavjhingran@gmail.com>
2016-11-25 18:32:25 +0530

    Micropost refinements

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -25,4 +25,8 @@ class MicropostTest < ActiveSupport::TestCase
   	@micropost.content = 'a' * 141
   	assert_not @micropost.valid?
   end
+
+  test "order of microposts should be latest first" do
+  	assert_equal microposts(:most_recent), Micropost.first
+  end
 end
