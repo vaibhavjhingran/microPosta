d317b7a7b61424bca5f751328f90818686ed344c
Vaibhav Jhingran <vaibhavjhingran@gmail.com>
2016-11-25 20:33:10 +0530

    Displaying microposts

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,6 +16,13 @@ User.create!(name: "Admin Man",
 				 password_confirmation: password,
 				 activated: true,
 				 activated_at: Time.zone.now)
+
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.microposts.create!(content: content) }
 end
+end
+
 
 
