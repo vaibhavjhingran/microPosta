0cecf27aadd31eab8c4309b691e19ad1579f5f66
Vaibhav Jhingran <vaibhavjhingran@gmail.com>
2016-11-26 21:17:12 +0530

    Adding user feed to home view

--- app/models/user.rb
+++ app/models/user.rb
@@ -62,6 +62,10 @@ class User < ApplicationRecord
   	update_attribute(:remember_digest, nil)
   end
 
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
+
   private
     def downcase_email
       self.email = email.downcase
