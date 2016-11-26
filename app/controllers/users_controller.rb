6f81fc176e63b6f8d76aeca2d4f1a0aff333730c
Vaibhav Jhingran <vaibhavjhingran@gmail.com>
2016-11-26 20:45:42 +0530

    Modifying microposts
    * Moved logged_in_user to Application Controller for base case uses.
    * Modified user_controller for the same.
    * Added some tests for micropost controller.
    * Moded micropost controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
 
@@ -56,13 +56,13 @@ class UsersController < ApplicationController
   									 :password_confirmation)
   	  end
 
-  	  def logged_in_user
-  	  	unless logged_in?
-  	  		store_location
-  	  		flash[:danger] = "Please log in to continue."
-  	  		redirect_to login_url
-  	  	end
-  	  end
+  	  # def logged_in_user
+  	  # 	unless logged_in?
+  	  # 		store_location
+  	  # 		flash[:danger] = "Please log in to continue."
+  	  # 		redirect_to login_url
+  	  # 	end
+  	  # end
 
   	  def correct_user
   	  	@user = User.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
 
-  # get 'password_resets/new'
-
-  # get 'password_resets/edit'
-
   root   'static_pages#home'
   get    '/help',    to: 'static_pages#help'
   get    'about',    to: 'static_pages#about'
@@ -15,5 +11,6 @@ Rails.application.routes.draw do
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets, 	  only: [:new, :edit, :create, :update]
+  resources :microposts,          only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
