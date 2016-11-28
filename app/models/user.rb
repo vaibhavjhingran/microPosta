3ffeb677099e6a613051ccd5832067c050e29abe
Vaibhav Jhingran <vaibhavjhingran@gmail.com>
2016-11-28 12:30:26 +0530

    Adding case for followed users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name: "Relationship", 
                                   foreign_key: "follower_id", 
                                   dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
 
   attr_accessor :remember_token, :activation_token, :reset_token
   
@@ -67,6 +68,18 @@ class User < ApplicationRecord
 
   def feed
     Micropost.where("user_id = ?", id)
+  end
+
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
   end
 
   private
